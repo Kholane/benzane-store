7166c80d964828609b65ed57fd5ea9fb1f159cda
ColeColombia <Benzemabenelzane@yahoo.com>
2022-07-15 23:35:09 +0200

    fix empty url

--- src/Shop.js
+++ src/Shop.js
@@ -15,7 +15,7 @@ export default class Shop extends React.Component{
     render(){
         return (
         <>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="shop" element={<Catalog/>} />
